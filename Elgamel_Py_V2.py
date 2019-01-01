b54e57dec851ccfbce84e965d5b1bf91b1045c5f
Bradley Soo <38500505+Themathematician314@users.noreply.github.com>
2018-12-31 23:50:48 -0500

    Update Elgamel_Py_V2.py

--- Elgamel_Py_V2.py
+++ Elgamel_Py_V2.py
@@ -7,4 +7,28 @@ import random
 
 class Public_Key(object):
   
-  def __init__(self, 
+  #prime is the prime value of the space that we are working in, and g is the 
+  def __init__(self, prime=None, g=None, x=None):
+    self.prime = prime
+    self.g = g
+    self.x = x
+ 
+class Private_Key(object):
+
+  def __init__(self, prime=None, g=None, x=None):
+    self.prime = prime
+    self.g = g
+    self.x = x
+    
+#These functions are needed. We assume that a > b
+def gcd(a, b):
+  while b != 1:
+    c = a % b
+    a = b
+    b = c
+  return a
+
+def exp_modulus(a, b, c):
+  return pow(a, b, c)
+
+def
