d8c16511af68e59899861a541b12588ace87ff3f
Bradley Soo <38500505+Themathematician314@users.noreply.github.com>
2019-01-04 16:53:06 -0500

    Create DLP.py

---
+++ DLP.py
@@ -0,0 +1,11 @@
+def main():
+  
+    g = int(input("Enter a number: "))
+    prime = int(input("Enter a number: "))
+    target = int(input("Enter a number: "))
+    for i in range(prime):
+        if g**i % prime == target:
+            return i
+    print(i)
+   
+main()
